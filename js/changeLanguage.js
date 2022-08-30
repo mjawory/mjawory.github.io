c259f1436654fe1a545e3fb30710a08d7de9dfe6
gonzalojaimovitch <mjawory@gmail.com>
2022-08-30 23:20:12 +0200

    First commit

---
+++ js/changeLanguage.js
@@ -0,0 +1,130 @@
+var language = {
+    menu_home: {en: "HOME", pl: "HOME", es: "INICIO"},
+    menu_for_whom: {en: "FOR WHOM", pl: "DLA KOGO", es: "PARA QUIÉN"},
+    menu_about: {en: "ABOUT ME", pl: "O MNIE", es: "SOBRE MÍ"},
+    menu_how: {en: "HOW", pl: "FORMA PRACY", es: "METODOLOGÍA"},
+    menu_contact: {en: "CONTACT", pl: "KONTAKT", es: "CONTACTO"},
+    baner_1_title: {en: "<br><br><br>PSYCHOTERAPY", pl: "<br><br><br>GABINET PSYCHOTERAPII", es: "<br><br><br>GABINETE DE PSICOTERAPIA"},
+    baner_1_button: {en: "Read more", pl: "Zobacz więcej", es: "Leer más"},
+    baner_2_individual: {en: "Individual Therapy", pl: "Terapia Indywidualna", es: "Terapia Individual"},
+    baner_2_online: {en: "On-line Therapy", pl: "Terapia On-line", es: "Terapia On-line"},
+    baner_2_youth: {en: "Adolescence Therapy", pl: "Terapia Młodzieży", es: "Terapia con Adolescentes"},
+    baner_2_consultation: {en: "Consultation", pl: "Konsultacje", es: "Consulta"},
+    baner_2_button: {en: "Read more", pl: "Zobacz więcej", es: "Leer más"},
+    baner_3_text: {en: "Call me to schedule the first session", pl: "Przed wizytą proszę o kontakt telefoniczny", es: "Por favor, póngase en contacto por teléfono para concertar la primera sesión"},
+    baner_3_phone: {en: "tel. +48 508 56 24 56", pl: "tel. 508 56 24 56", es: "tel. +48 508 56 24 56"},
+    baner_3_button: {en: "Read more", pl: "Zobacz więcej", es: "Leer más"},
+    for_whom_title: {en: "For whom", pl: "Dla kogo", es: "Para quién"},
+    for_whom_desc: {en: "The office welcome those who:", pl: "Do gabinetu zapraszam osoby, które:", es: "Están invitados a la consulta quienes:"},
+    for_whom_col1_1_title: {en: "Emotions", pl: "Emocje", es: "Emociones"},
+    for_whom_col1_1_point1: {en: "are experiencing emotional crises", pl: "przeżywają kryzysy emocjonalne", es: "están experimentando crisis emocionales"},
+    for_whom_col1_1_point2: {en: "feel a lack of control over their own lives and their own emotions (e.g. outbursts of anger)", pl: "mają poczucie braku kontroli nad własnym życiem i własnymi emocjami (np. wybuchy gniewu)", es: "sienten una falta de control sobre su propia vida y sus propias emociones (por ejemplo, arrebatos de ira)"},
+    for_whom_col1_2_title: {en: "Self-evaluation", pl: "Samoocena", es: "Autoevaluación"},
+    for_whom_col1_2_point1: {en: "have low self-esteem", pl: "posiadają niskie poczucie własnej wartości", es: "tienen una baja autoestima"},
+    for_whom_col1_2_point2: {en: "worry that they can't cope with anything", pl: "martwią się, że z niczym sobie nie poradzą", es: "se preocupan por no poder hacer frente a nada"},
+    for_whom_col1_3_title: {en: "Work", pl: "Praca", es: "Trabajo"},
+    for_whom_col1_3_point1: {en: "are experiencing stress and problems related to work or study", pl: "przeżywają stresy i problemy związane z pracą zawodową lub nauką", es: "sufren estrés y problemas relacionados con el trabajo o los estudios"},
+    for_whom_col1_3_point2: {en: "feel „burnt out” professionally", pl: "czują się „wypalone” zawodowo", es: "se sienten „quemados” profesionalmente"},
+    for_whom_col1_4_title: {en: "CRISIS", pl: "KRYZYS", es: "CRISIS"},
+    for_whom_col1_4_point1: {en: "are in a crisis situation - painful experiences", pl: "znajdują się w sytuacji kryzysowej – bolesne przeżycia", es: "se encuentran en una situación de crisis - experiencias dolorosas"},
+    for_whom_col1_4_point2: {en: "have lost a loved one (bereavement, divorce, separation)", pl: "utraciły bliską osobę (żałoba, rozwód, rozstanie)", es: "han perdido a un ser querido (duelo, divorcio, separación)"},
+    for_whom_col2_1_title: {en: "Relationships", pl: "Związki", es: "Relaciones"},
+    for_whom_col2_1_point1: {en: "experience difficulties in being with another person", pl: "doświadczają trudności w byciu z drugą osobą", es: "experimentan dificultades para estar con otra persona"},
+    for_whom_col2_1_point2: {en: "have difficulties building a happy relationship", pl: "mają problem ze zbudowaniem szczęśliwego związku", es: "tienen dificultades para construir una relación feliz"},
+    for_whom_col2_1_point3: {en: "are experiencing difficulties in their family life", pl: "przeżywają trudności w życiu rodzinnym", es: "tienen dificultades en su vida familiar"},
+    for_whom_col2_2_title: {en: "PSYCHOSEXUAL ORIENTATION", pl: "ORIENTACJA PSYCHOSEKSUALNA", es: "ORIENTACIÓN PSICOSEXUAL"},
+    for_whom_col2_2_point1: {en: "are struggling with the acceptance of their psychosexual orientation", pl: "borykają się z akceptacją swojej orientacji psychoseksualnej", es: "sufren en la aceptación de su orientación psicosexual"},
+    for_whom_col2_2_point2: {en: "have any doubts about it", pl: "mają wątpliwości z nią związane", es: "tienen dudas al respecto"},
+    for_whom_col2_3_title: {en: "LONELINESS", pl: "SAMOTNOŚĆ", es: "SOLEDAD"},
+    for_whom_col2_3_point1: {en: "experience existential problems: loneliness, isolation, lack of sense of life, fear of death, emptiness", pl: "doświadczają problemów egzystencjalnych: samotność, izolacja, brak poczucia sensu życia, lęk przed śmiercią, pustka", es: "experimentan problemas existenciales: soledad, aislamiento, falta de sentido de la vida, miedo a la muerte, vacío"},
+    for_whom_col2_3_point2: {en: "are suffering and at the same time are unable to identify the causes of their suffering", pl: "doznają cierpienia a jednocześnie nie potrafią określić jego przyczyn", es: "sufren y al mismo tiempo son incapaces de identificar las causas de su sufrimiento"},
+    for_whom_col2_4_title: {en: "SELF-DEVELOPMENT", pl: "SAMOROZWÓJ", es: "DESARROLLO PERSONAL"},
+    for_whom_col2_4_point1: {en: "want to develop and improve their quality of life", pl: "chcą się rozwijać i poprawiać jakość swojego życia", es: "quieren desarrollar y mejorar su calidad de vida"},
+    for_whom_col2_4_point2: {en: "want to build healthy and good relationships with other people", pl: "chcą budować zdrowe i dobre relacje z innymi ludźmi", es: "quieren establecer relaciones sanas y buenas con otras personas"},
+    about_title: {en: "About me", pl: "O mnie", es: "Sobre mí"},
+    about_text_p1: {en: `I completed my comprehensive (4-year) training in psychotherapy at the Institute of Integral Gestalt Psychotherapy in Cracow, which is a centre accredited by EAGT - European Association for Gestalt Therapy. I also gained professional experience during my clinical internship under the supervision of Prof. Łukasz Święcicki at the Department of Affective Disorders at the Institute of Psychiatry and Neurology in Warsaw.
+                    <br>
+                    <br>
+                    In addition to my training in psychotherapy, I also completed a workshop recommended by the Polish Society of Sexology: "European Sexual Diversity Training". I am one of the therapists who form a network at KPH to provide psychological support to LGBTQ people and their families: https://kph.org.pl/pomoc/pomoc-psychologiczna/.
+                    <br>
+                    <br>
+                    Taking care of the high quality of my work, I am subject to constant supervision and am guided by the Code of Ethics of the Polish Society of Gestalt Psychotherapy. My first degree is a Doctor of Humanities in Philosophy. I have been working at the Institute of Philosophy at UWM in Olsztyn for over 10 years.
+                    <br>
+                    <br>
+                    Training and conferences:`,
+
+                    pl: `Całościowe (4-letnie) szkolenie z zakresu psychoterapii ukończyłem w Instytucie Integralnej
+                    Psychoterapii Gestalt w Krakowie, który jest ośrodkiem akredytowanym przez EAGT - European
+                    Association for Gestalt Therapy (Europejskie Stowarzyszenie Terapii Gestalt). Doświadczenie
+                    zawodowe zdobywałem również podczas stażu klinicznego, który odbyłem pod okiem prof. Łukasza
+                    Święcickiego na Oddziale Chorób Afektywnych w Instytucie Psychiatrii i Neurologii w
+                    Warszawie.
+                    <br>
+                    <br>
+                    Poza szkoleniami z zakresu psychoterapii ukończyłem również warsztat rekomendowany przez
+                    Polskie Towarzystwo Seksuologiczne: „European Sexual Diversity Training”. Należę do
+                    terapeutów, którzy tworzą przy KPH sieć osób udzielających wsparcia psychologicznego osobom
+                    LGBTQ i ich rodzinom: https://kph.org.pl/pomoc/pomoc-psychologiczna/
+                    <br>
+                    <br>
+                    Dbając o wysoką jakość swojej pracy podlegam stałej superwizji oraz kieruję się Kodeksem
+                    Etyki Polskiego Towarzystwa Psychoterapii Gestalt. Z pierwszego wykształcenia jestem doktorem nauk humanistycznych w zakresie filozofii. Od
+                    ponad 10 lat pracuję w Instytucie Filozofii UWM w Olsztynie.
+                    <br>
+                    <br>
+                    Szkolenia i konferencję:`,
+                    es: `
+                    Completé mi formación integral (4 años) en psicoterapia en el Instituto de Psicoterapia Gestalt Integral de Cracovia, un centro acreditado por la EAGT - Asociación Europea de Terapia Gestalt. Además, adquirí experiencia profesional durante mis prácticas clínicas bajo la supervisión del profesor Łukasz Święcicki en el Departamento de Trastornos Afectivos del Instituto de Psiquiatría y Neurología de Varsovia.
+                    <br>
+                    <br>
+                    Además de mi formación en psicoterapia, también completé un taller recomendado por la Sociedad Polaca de Sexología: "Formación europea en diversidad sexual". Soy uno de los terapeutas que forman una red en KPH para proporcionar apoyo psicológico a las personas LGBTQ y sus familias: https://kph.org.pl/pomoc/pomoc-psychologiczna/.
+                    <br>
+                    <br>
+                    Velando por la alta calidad de mi trabajo, estoy sometida a una supervisión constante y me guío por el Código Ético de la Sociedad Polaca de Psicoterapia Gestalt. Mi primer título es el de Doctor en Humanidades en Filosofía. Llevo más de 10 años trabajando en el Instituto de Filosofía de la UWM en Olsztyn.
+                    <br>
+                    <br>
+                    Formación y disertaciones:`},
+    about_bullet_1: {en: "'The university towards students with mental disorders'", pl: "'Uczelnia wobec studentów z zaburzeniami psychicznymi'", es: "'La universidad ante los estudiantes con trastornos mentales'"},
+    about_bullet_2: {en: "workshop 'Phenomenology of Situations or Relationships in Gestalt Therapy Gestalt' conducted by the director of the Gestalt Institute in Oslo - Daan van Baalen", pl: "warsztat 'Fenomenologia sytuacji lub relacji w Terapii Gestalt' prowadzony przez dyrektora Instytutu Gestalt w Oslo - Daana van Baalena", es: "taller 'Fenomenología de las situaciones o de las relaciones en la terapia gestáltica Gestalt' dirigido por el director del Instituto Gestalt de Oslo - Daan van Baalen"}, 
+    about_bullet_3: {en: "philosophical-psychiatric seminar 'Relationships'", pl: "seminarium filozoficzno-psychiatryczne pt. 'Relacje'", es: "seminario filosófico-psiquiátrico 'Relaciones'"},
+    about_bullet_4: {en: "scientific conference 'Around the person of the psychotherapist. Clinical Reflection and Research Results' (Rasht Institute for Group Analysis)", pl: "konferencja naukowa 'Wokół osoby psychoterapeuty. Refleksja kliniczna i wyniki badań naukowych' (Instytu Analizy Grupowej 'Rasztów')", es: "conferencia científica 'En torno a la persona del psicoterapeuta. Reflexión clínica y resultados de la investigación' (Instituto Rasht de Análisis Grupal)"},
+    about_bullet_5: {en: "workshop in the field of sexuality entitled. 'Therapeutic Work in the Erotic Field' conducted by Dr. Adam Kincla (Association for Advancements in Gestalt Therapy)", pl: "warsztat z zakresu seksualności pt. 'Praca terapeutyczna w obszarze pola erotycznego' prowadzony przez dr Adama Kincla (Association for Advancements in Gestalt Therapy)", es: "taller en el ámbito de la sexualidad titulado. 'Trabajo terapéutico en el campo erótico' dirigido por el Dr. Adam Kincla (Asociación para el Avance de la Terapia Gestalt)"},
+    about_button: {en: "FOR WHOM", pl: "DLA KOGO", es: "PARA QUIÉN"},
+    how_title: {en: "HOW", pl: "FORMA PRACY", es: "METODOLOGÍA"},
+    how_p1: {en: "<font size='4'>Gestalt Psycotherapy", pl: "<font size='4'>Psychoterapia Gestalt", es: "<font size='4'>Psicoterapia Gestalt"},
+    how_p2: {en: "For me, psychotherapy is an encounter in which the person coming to the office seeks, with the support of the psychotherapist, new ways of looking at the world and at his or her situation in it. This is how the possibility of reducing suffering and making life more comfortable by strengthening one's potential is created. The overriding aim of the therapy is to achieve maturity by gaining greater awareness and thus strengthening one's needs and the ability to meet them in such a way that they are heard in relationships with other people.", pl: "Psychoterapia jest dla mnie spotkaniem, w którym osoba przychodzącą do gabinetu poszukuje przy wsparciu psychoterapeuty nowych możliwości spojrzenia na świat i na swoją w nim sytuację. Tak powstaje możliwość pomniejszenia cierpienia i podniesienia komfortu życia poprzez wzmocnienie swojego potencjału. Nadrzędnym celem terapii jest osiągnięcie dojrzałości przez zyskanie większej świadomości, a co za tym idzie wzmocnienie swoich potrzeb i umiejętności ich zaspokajania w taki sposób, aby były one słyszane w relacjach z innymi ludźmi.", es: "Para mí, la psicoterapia es un encuentro en el que la persona que acude a la consulta busca, con el apoyo del psicoterapeuta, nuevas formas de ver el mundo y su situación en él. Así se crea la posibilidad de reducir el sufrimiento y hacer la vida más cómoda fortaleciendo el propio potencial. El objetivo primordial de la terapia es alcanzar la madurez adquiriendo una mayor conciencia y fortaleciendo así las propias necesidades y la capacidad de satisfacerlas de manera que sean escuchadas en las relaciones con otras personas."},
+    how_card1_title: {en: "Individual Psychoterapy", pl: "Psychoterapia indywidualna", es: "Psicoterapia Individual"},
+    how_card1_text: {en: "These are one-to-one meetings in 50-minute sessions that take place once a week.", pl: "Są to spotkania indywidualna podczas 50 min. sesji, które odbywają się raz w tygodniu.", es: "Se trata de reuniones individuales en sesiones de 50 minutos que tienen lugar una vez a la semana."},
+    how_card2_title: {en: "Consultation", pl: "Konsultacje", es: "Consulta"},
+    how_card2_text: {en: "One or more meetings with a therapist with the aim of identifying the problem and helping to find ways to solve it.", pl: "Jedno lub kilka spotkań z terapeutą, których celem jest zidentyfikowania problemu oraz pomoc w znalezieniu dróg jego rozwiązania.", es: "Una o más reuniones con un terapeuta para identificar el problema y ayudarle a encontrar la manera de solucionarlo."},
+    how_card3_title: {en: "Adolescency Psycotherapy", pl: "Psychoterapia młodzieży", es: "Psicoterapia con Adolescentes"},
+    how_card3_text: {en: "These are one-to-one meetings with people in the throes of adolescence, a time of developmental crisis which brings with it various difficulties and dilemmas. At that time, the adolescent may experience incomprehensible moods, disturbing behaviour or a variety of violent somatic and emotional reactions. Work begins after consultation with the adolescent and parents.", pl: "Są to spotkania indywidualne z osobami będącymi w okresie dojrzewania, czyli w czasie kryzysu rozwojowego, który przynosi ze sobą różne trudności i dylematy. Młody człowiek może doświadczać wtedy niezrozumiałych nastrojów, niepokojących zachowań, czy różnorodnych, gwałtownych reakcji somatycznych i emocjonalnych. Praca rozpoczyna się po konsultacji z adolescentem i rodzicami.", es: "Se trata de encuentros individuales con personas en plena adolescencia, una época de crisis evolutiva que conlleva diversas dificultades y dilemas. En ese momento, el adolescente puede experimentar estados de ánimo incomprensibles, comportamientos perturbadores o una variedad de reacciones somáticas y emocionales violentas. El trabajo se inicia tras la consulta con el adolescente y los padres."},
+    contact_title: {en: "CONTACT ME", pl: "KONTAKT ZE MNĄ", es: "CONTÁCTAME"},
+    contact_subtitle: {en: "<font size='4px'>Please call in advance before visiting.", pl: "<font size='4px'>Przed wizytą proszę o wcześniejszy kontakt telefoniczny.", es: "<font size='4px'>Se ruega llamar con antelación antes de la visita."},
+    contact_address_title: {en: "Address", pl: "Adres", es: "Dirección"},
+    contact_address_1: {en: "<font size='4px'>ul. Kościuszki 13", pl: "<font size='4px'>ul. Kościuszki 13", es: "<font size='4px'>ul. Kościuszki 13"},
+    contact_address_2: {en: "<font size='4px'>gabinet nr 318 (III piętro)", pl: "<font size='4px'>gabinet nr 318 (III piętro)", es: "<font size='4px'>gabinet nr 318 (III piętro)"},
+    contact_address_3: {en: "<font size='4px'>10-501 Olsztyn", pl: "<font size='4px'>10-501 Olsztyn", es: "<font size='4px'>10-501 Olsztyn"},
+    contact_phone_title: {en: "Phone", pl: "Telefon", es: "Teléfono"},
+    contact_phone: {en: "<font size='4px'>+48 508 562 456", pl: "<font size='4px'>+48 508 562 456", es: "<font size='4px'>+48 508 562 456"}
+};
+
+function updateLanguage(value) {
+    if(value === "en") {
+        for (var key in language){
+            document.getElementById(key).innerHTML = language[key].en;
+        }
+    }
+
+    if(value === "pl") {
+        for (var key in language){
+            document.getElementById(key).innerHTML = language[key].pl;
+        }
+    }
+
+    if(value === "es") {
+        for (var key in language){
+            document.getElementById(key).innerHTML = language[key].es;
+        }
+    }
+}
